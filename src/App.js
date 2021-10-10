9d78c580cfe1cdd57a4362daa34057cbf7849f18
k.shalguev <k.shalguev@yandex.ru>
2021-10-10 12:18:49 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from './components/card/card';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Card/>
+    );
+  }
+}
---
+++ src/components/card/card.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Card  = () => {
+
+  return (
+    <div className={'card'}>
+      <h1>тест!</h1>
+    </div>
+  );
+}
